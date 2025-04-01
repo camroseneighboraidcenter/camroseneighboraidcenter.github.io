2e80f4c9132e0e4f7f13333f5275715e478c323c
Jarett <jarett.rude@gmail.com>
2025-03-31 22:48:35 -0700

    Add GitHub Actions workflow for deployment to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
-  site: "https://camroseneighboraidcenter.ca",
+  site: "https://camroseneighboraidcenter.github.io",
   compressHTML: true,
   integrations: [mdx()],
   build: {
